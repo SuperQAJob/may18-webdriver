5a0368bc8ffddc86510b94281c4c73484d02348c
SuperSQA <SuperQAJob@gmail.com>
2021-08-06 19:12:35 -0500

    Reformat Code uiTest.js

--- test/uiTest.js
+++ test/uiTest.js
@@ -1,22 +1,18 @@
 import {Builder, By, Key, until} from "selenium-webdriver";
-import ChromeDriver from 'chromedriver'
 
-describe("", () =>)
+describe("", () =>
 {
-    it("", () =>)
+    it("", async() =>
     {
         let driver = await new Builder().forBrowser('chrome').build();
 
         try {
-            await driver.get('http://www.google.com/ncr');
+            await driver.get('https://www.google.com');
             await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
             await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
         } finally {
             await driver.quit();
         }
-    }
-)
-;
-})
-;
+    });
+});
 
